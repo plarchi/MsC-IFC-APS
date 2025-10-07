44af3711ae4e2ed7ef3a9bcc9448f85be64c075d
plarchi <plarchi@connect.hku.hk>
2025-10-07 11:12:28 +0200

    Import local project snapshot

---
+++ Models/APS.Auth.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Autodesk.Authentication;
+using Autodesk.Authentication.Model;
+
+public record Token(string AccessToken, DateTime ExpiresAt);
+
+public partial class APS
+{
+    private Token _internalTokenCache;
+    private Token _publicTokenCache;
+
+    private async Task<Token> GetToken(List<Scopes> scopes)
+    {
+        var authenticationClient = new AuthenticationClient();
+        var auth = await authenticationClient.GetTwoLeggedTokenAsync(_clientId, _clientSecret, scopes);
+        return new Token(auth.AccessToken, DateTime.UtcNow.AddSeconds((double)auth.ExpiresIn));
+    }
+
+    public async Task<Token> GetPublicToken()
+    {
+        if (_publicTokenCache == null || _publicTokenCache.ExpiresAt < DateTime.UtcNow)
+            _publicTokenCache = await GetToken([Scopes.ViewablesRead]);
+        return _publicTokenCache;
+    }
+
+    private async Task<Token> GetInternalToken()
+    {
+        if (_internalTokenCache == null || _internalTokenCache.ExpiresAt < DateTime.UtcNow)
+            _internalTokenCache = await GetToken([Scopes.BucketCreate, Scopes.BucketRead, Scopes.DataRead, Scopes.DataWrite, Scopes.DataCreate]);
+        return _internalTokenCache;
+    }
+}
