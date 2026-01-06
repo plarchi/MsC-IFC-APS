9cffa86fc3c03910b6acd09957dfd39e5db18aa9
DESKTOP-T0REN6D\Ping <plarchi@connect.hku.hk>
2026-01-06 21:39:46 +0000

    Testing and JSON export.

---
+++ PyDataTransform/Test.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+import json
+from pathlib import Path
+from typing import Any
+
+DEFAULT_INPUT = Path("JSON Whole Model") / "ASTIDC-STAN-HE-EPD-MVB1-M-E-0002.json"
+
+print(DEFAULT_INPUT)
